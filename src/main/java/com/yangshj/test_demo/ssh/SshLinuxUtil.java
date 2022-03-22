cfbda1f445f85b0e9f8af2df205fe65d4fd7e6b4
yangshj <yangshj@teamsun.com.cn>
2022-03-22 09:24:25 +0800

    update linux collect

--- src/main/java/com/yangshj/test_demo/ssh/SshLinuxUtil.java
+++ src/main/java/com/yangshj/test_demo/ssh/SshLinuxUtil.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import ch.ethz.ssh2.Connection;
@@ -21,22 +22,54 @@ public class SshLinuxUtil {
     public static void main(String[] args) {
         SshLinuxUtil util = new SshLinuxUtil();
 
-        double a = util.getMemoryRate();
+        // 内存利用率
+//        double a = util.getMemoryRate();
+//        System.out.println("内存利用率: " + a + "%");
 
-        System.out.println("内存利用率: " + a + "%");
+
+        // cpu利用率
+//        long[] r1 = util.getCpuRate();
+//        try {
+//            Thread.sleep(500);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        long[] r2 = util.getCpuRate();
+//
+//        long allTime = r2[0] - r1[0];
+//        long idle = r2[1] - r1[1];
+//        long used = allTime - idle;
+//        System.out.println("allTime: " + allTime);
+//        System.out.println("idle: " + idle);
+//        double f1 = new BigDecimal((float) used / allTime).setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue() * 100;
+//        System.out.println("cpu利用率： " + f1 + "%");
     }
 
-    private double getCpuRate() {
+    private long[] getCpuRate() {
         String shell = String.format("%s", "cat /proc/stat");
 
         List<String> result = execCmd(shell);
         for (String s : result) {
             System.out.println("result===========> " + s);
         }
+        String[] cpu = result.get(0).split("\\s+");
+        List<String> cpuList1 = Arrays.asList(cpu);
+        List<String> cpuList = new ArrayList<>(cpuList1);
+        for (String a : cpuList) {
+            System.out.println("=========> " + a);
+        }
+        cpuList.remove(0);
 
-        String[] aa = result.get(0).split("\\s+");
+        long allTime = 0L;
+        for (String s : cpuList) {
+            allTime += Long.parseLong(s);
+        }
+        System.out.println(String.format("CPU使用总时间 = %s;idle=%s", allTime, cpuList.get(3)));
 
-        return 0;
+        long[] r = new long[2];
+        r[0] = allTime;
+        r[1] = Long.parseLong(cpuList.get(3));
+        return r;
     }
 
     private double getMemoryRate() {
